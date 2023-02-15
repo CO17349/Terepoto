fe92bd6a8827890b909fe72564914145be4f48f0
CO17349 <ripjeet7777@gmail.com>
2023-02-15 14:39:10 -0600

    Text amendment functionality added

--- src/TextFieldDemo.java
+++ src/TextFieldDemo.java
@@ -1,2 +1,28 @@
-public class TextFieldDemo {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class TextFieldDemo extends RadioButtonDemo{
+
+    @Override
+    protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+
+        BorderPane paneForTextField = new BorderPane();
+        paneForTextField.setPadding(new Insets(5, 5, 5, 5));
+        paneForTextField.setStyle("-fx-border-color: green");
+        paneForTextField.setLeft(new Label("Enter a new message"));
+
+        TextField txtMessage = new TextField();
+        txtMessage.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForTextField.setCenter(txtMessage);
+        pane.setTop(paneForTextField);
+
+        txtMessage.setOnAction(e -> {
+            text.setText(txtMessage.getText());
+        });
+        return pane;
+    }
 }
